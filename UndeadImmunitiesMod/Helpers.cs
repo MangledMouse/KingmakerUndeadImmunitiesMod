f77780f3c5e6593b5a21b5ae7091f1f164f0719e
Chris <ctourlouki@gmail.com>
2020-05-04 11:50:44 -0400

    Setting Up still issue with csproj
    I am crashing on a line that says this
    "Your project does not reference ".NETFramework,Version=v4.7" framework. Add a reference to ".NETFramework,Version=v4.7" in the "TargetFrameworks" property of your project file and then re-run NuGet restore."
    Unsure why since the project file does the same thing as other working projfiles in reference to that

---
+++ UndeadImmunitiesMod/Helpers.cs
@@ -0,0 +1,18 @@
+﻿using Kingmaker.Blueprints;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UndeadImmunitiesMod
+{
+    class Helpers
+    {
+    }
+    static class ExtensionMethods
+    {
+        public static T Get<T>(this LibraryScriptableObject library, String assetId) where T : BlueprintScriptableObject
+        {
+            return (T)library.BlueprintsByAssetId[assetId];
+        }
+    }
+}
--- UndeadImmunitiesMod/Main.cs
+++ UndeadImmunitiesMod/Main.cs
@@ -1,8 +1,51 @@
-﻿using System;
+﻿using Kingmaker.Blueprints;
+using System;
+using UnityModManagerNet;
 
 namespace UndeadImmunitiesMod
 {
     public class Main
     {
+        public static LibraryScriptableObject library;
+        internal static UnityModManagerNet.UnityModManager.ModEntry.ModLogger logger;
+
+        [System.Diagnostics.Conditional("DEBUG")]
+        internal static void DebugLog(string msg)
+        {
+            if (logger != null) logger.Log(msg);
+        }
+
+        static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            try
+            {
+                logger = modEntry.Logger;
+                harmony = Harmony12.HarmonyInstance.Create(modEntry.Info.Id);
+                harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+            }
+            catch (Exception ex)
+            {
+                DebugError(ex);
+                throw ex;
+            }
+            return true;
+        }
+
+        [Harmony12.HarmonyPatch(typeof(LibraryScriptableObject), "LoadDictionary")]
+        [Harmony12.HarmonyPatch(typeof(LibraryScriptableObject), "LoadDictionary", new Type[0])]
+        static class LibraryScriptableObject_LoadDictionary_Patch
+        {
+            static void Postfix(LibraryScriptableObject __instance)
+            {
+                var self = __instance;
+                if (Main.library != null)
+                    return;
+                Main.library = self;
+
+                //The codes!!!
+                ImmunitiesChanger.undeadImmunitiesChange();
+            }
+        }
     }
 }
